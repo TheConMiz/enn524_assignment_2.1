a1000c7897f2d538c7f77f0ddc2f40be8e33404d
Vishal Venkatraman <ihandsomeguy97@gmail.com>
2021-10-11 01:04:57 +0800

    Question 2.6 - Attempt

--- client.py
+++ client.py
@@ -23,30 +23,57 @@ def on_connect(client, userdata, flags, rc):
     smallMessage = smallFile.readlines()
     largeMessage = largeFile.readlines()
 
-    # Publish messages to those 3 topics
-    client.publish(topic1, str(time.time()))
-    # client.publish(topic2, str(smallMessage))
-    # client.publish(topic3, str(largeMessage))
+    # Define a timer of 600 seconds. 
+    timeCount = 10
+
+    while timeCount > 0:
+        # Publish messages to those 3 topics
+        # Since the timestamp value changes each time it is sent, it must only be sent once. Hence, QoS = 1
+        client.publish(topic1, str(time.time()), qos=1)
+        # Each of these messages must be sent at least once for the packet loss calculations, so QoS = 1
+        client.publish(topic2, str(smallMessage), qos=1)
+        # client.publish(topic3, str(largeMessage), qos=1)
+        # sdsd
+        timeCount -= 1
+        time.sleep(1)
+
+    client.publish(topic2, "done", qos=1)
+    client.publish(topic3, "done", qos=1)
 
 
 def on_message(client, userdata, msg):
-
+    # Count 10/100kb messages received. 
+    smallMessageCount = 0
+    largeMessageCount = 0
+    expectedMessageCount = 600
 
     if msg.topic == "message/rtd":
-        
+        # Get the current timestamp
         currentTime = time.time()
+        # Get the timestamp in the message
         sendTime = float(msg.payload.decode("utf-8"))
-
+        # Calculate the round trip time
         roundTripTime = currentTime - sendTime
-
+        # Summarise findings
         print("Message Received: " + msg.topic + "\n")
-
         print("Sent Timestamp: " + str(sendTime) + " seconds. \n")
         print("Received Timestamp: " + str(currentTime) + " seconds. \n")
         print("Round Trip Time: " + str(roundTripTime) + " seconds. \n")
 
-    else:
-        print("Message Received: " + msg.topic + "\n" + msg.payload.decode("utf-8"))
+    elif msg.topic == "message/10kb":
+        print("Topic: " + msg.topic)
+
+        if msg.payload.decode("utf-8") == "done":
+            packetLossRate = (expectedMessageCount - smallMessageCount) / 100
+
+            print(str(smallMessageCount) + " messages received with packet loss of " + str(packetLossRate) + "percent. \n")
+
+        else:
+            smallMessageSize = len(msg.payload)
+            print("Message received. Size: " + str(smallMessageSize) + "B")
+
+    # elif msg.topic == "message/100kb":
+
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print(str(userdata) + " " + str(granted_qos))
@@ -64,11 +91,6 @@ host = "192.168.0.221"
 # Mosquitto Broker listening on Port 1884
 port = 1884
 keepalive = 60
-
-topic1 = "message/rtd"
-topic2 = "message/10kb"
-topic3 = "message/100kb"
-
 
 client = mqtt.Client()
 
